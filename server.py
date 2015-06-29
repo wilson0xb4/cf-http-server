fe07b26ecbadecc708b1b54e8006709c9a1b1a06
Jim Grant <jimgrant@gmail.com>
2015-06-29 16:07:37 -0700

    constants stored in server.py. comparing request to constants. use constants in tests

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 import socket
 
+ADDR = ('127.0.0.1', 8001)
+OK_REQUEST = b"GET / HTTP/1.1"
+ERROR_REQUEST = b"Hey man how's it going"
+RESPONSE_200 = b"HTTP/1.1 200 OK"
+RESPONSE_500 = b"HTTP/1.1 500 Internal Server Error"
+
 
 def response_ok():
-    return b"HTTP/1.1 200 OK"
+    return RESPONSE_200
 
 
 def response_error():
-    return b"HTTP/1.1 500 Internal Server Error"
+    return RESPONSE_500
 
 
 def config_server():
-    ADDR = ('127.0.0.1', 8001)
-
     server = socket.socket(
         socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
     )
@@ -28,12 +32,18 @@ def start_server():
     while True:
         try:
             conn, addr = server.accept()
-            while True:
-                msg = conn.recv(16)
-                conn.sendall(msg)
-                if len(msg) < 16:
-                    conn.close()
-                    break
+            request = conn.recv(1024)
+            if request == OK_REQUEST:
+                conn.sendall(RESPONSE_200)
+            else:
+                conn.sendall(RESPONSE_500)
+
+            # while True:
+            #     msg = conn.recv(16)
+            #     conn.sendall(msg)
+            #     if len(msg) < 16:
+            #         conn.close()
+            #         break
         except KeyboardInterrupt:
             break
 
--- test_server.py
+++ test_server.py
@@ -3,12 +3,6 @@ import socket
 import server
 from multiprocessing import Process
 
-ADDR = ('127.0.0.1', 8001)
-OK_REQUEST = b"GET / HTTP/1.1"
-ERROR_REQUEST = b"Hey man how's it going"
-RESPONSE_200 = b"HTTP/1.1 200 OK"
-RESPONSE_500 = b"HTTP/1.1 500 Internal Server Error"
-
 # @pytest.fixture()
 # def server_process():
 #     process = Process(target=server.start_server())
@@ -51,7 +45,7 @@ def test_temp():
         socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
     )
     client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    client.connect(ADDR)
-    client.sendall(OK_REQUEST)
+    client.connect(server.ADDR)
+    client.sendall(server.OK_REQUEST)
     response = client.recv(1024)
-    assert response == RESPONSE_200
+    assert response == server.RESPONSE_200
