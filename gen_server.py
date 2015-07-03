6a6295c2f25dc10ed0d0acffb0c18b20fc6f9a61
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-02 17:17:56 -0700

    Change names

--- gen_server.py
+++ gen_server.py
@@ -50,6 +50,6 @@ if __name__ == '__main__':
     from gevent.server import StreamServer
     from gevent.monkey import patch_all
     patch_all()
-    server = StreamServer(('127.0.0.1', 10000), start_server)
+    gserver = StreamServer(('127.0.0.1', 10000), start_server)
     print('Starting echo server on port 10000')
-    server.serve_forever()
+    gserver.serve_forever()
